aa947b43f13542e6b5376edfec98cf8dd6486022
Evgeniy Tkachenko <et.coder@gmail.com>
2022-05-07 12:20:34 +0300

    feat: Добавлена cross_validate и доп. метрика

--- src/eval_sel/train.py
+++ src/eval_sel/train.py
@@ -5,6 +5,10 @@ import click
 import mlflow
 import mlflow.sklearn
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold, cross_val_predict
 
 from .data import get_dataset
 from .pipeline import create_pipeline
@@ -32,12 +36,6 @@ from .pipeline import create_pipeline
     show_default=True,
 )
 @click.option(
-    "--test-split-ratio",
-    default=0.2,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
-    show_default=True,
-)
-@click.option(
     "--use-scaler",
     default=True,
     type=bool,
@@ -59,24 +57,38 @@ def train(
     dataset_path: Path,
     save_model_path: Path,
     random_state: int,
-    test_split_ratio: float,
     use_scaler: bool,
     max_iter: int,
     logreg_c: float,
 ) -> None:
-    features_train, features_val, target_train, target_val = get_dataset(
-        dataset_path,
-        random_state,
-        test_split_ratio,
-    )
+    X, y = get_dataset(dataset_path)
+
+    kfold = KFold(n_splits=10, random_state=random_state, shuffle=True)
+
     with mlflow.start_run():
-        pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
-        pipeline.fit(features_train, target_train)
-        accuracy = accuracy_score(target_val, pipeline.predict(features_val))
+
         mlflow.log_param("use_scaler", use_scaler)
         mlflow.log_param("max_iter", max_iter)
         mlflow.log_param("logreg_c", logreg_c)
+        mlflow.log_param("random_state", random_state)
+
+        pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
+        predict = cross_val_predict(pipeline, X, y, cv=kfold)
+
+        accuracy = accuracy_score(y, predict)
+        precision = precision_score(y, predict, average='macro')
+        recall = recall_score(y, predict, average='macro')
+        mse = mean_squared_error(y, predict)
+
         mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("precision", precision)
+        mlflow.log_metric("recall", recall)
+        mlflow.log_metric("mse", mse)
+
         click.echo(f"Accuracy: {accuracy}.")
+        click.echo(f"precision: {precision}.")
+        click.echo(f"Recall: {recall}.")
+        click.echo(f"MSE: {mse}.")
+
         dump(pipeline, save_model_path)
         click.echo(f"Model is saved to {save_model_path}.")
